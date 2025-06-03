f816b8bcd52f1838fe76105bb63a3638be56fc9a
Andres Henao <andres.henao@triduum.us>
2025-06-03 16:59:57 -0500

    update colors dashboard

--- lua/gentleman_kanagawa_blur/groups/integrations/snacks.lua
+++ lua/gentleman_kanagawa_blur/groups/integrations/snacks.lua
@@ -3,9 +3,9 @@ local p = require("gentleman_kanagawa_blur.palette")
 return {
 	SnacksIndent = { fg = p.gray3 },
 	SnacksIndentScope = { fg = p.subtext4 },
-	SnacksDashboardHeader = { fg = p.ligh_blue },
+	SnacksDashboardHeader = { fg = p.purple },
 	SnacksDashboardDesc = { fg = p.subtext1 },
-	SnacksDashboardKey = { fg = p.bright_red, bold = true },
+	SnacksDashboardKey = { fg = p.red, bold = true },
 	SnacksDashboardIcon = { fg = p.subtext1 },
 	SnacksDashboardFooter = { fg = p.purple, italic = true },
 	SnacksDashboardSpecial = { fg = p.bright_purple, bold = true, italic = true },
